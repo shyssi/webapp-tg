abff0be422693ca6c8dc45c8779128787968f335
a99icted <inferiorsad@gamil.com>
2026-08-14 10:18:47 +0700

    test appы

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 const tg = window.Telegram.WebApp;
 
 function App() {
+  const [user, setUser] = useState(null);
   useEffect(() => {
-    tg.ready();
+    tg?.ready();
+
+    const telegramUser = tg?.initDataUnsafe.user;
+
+    if (telegramUser) {
+      setUser(telegramUser);
+    }
+
 
   }, [])
 
@@ -14,6 +22,11 @@ function App() {
 
   return (
     <div className="App">
+
+      <p>
+        Hi {user.username ? user.username : user.first_name}
+      </p>
+      Your ID: {user.id}!
       <button onClick={onClose}>
         working soon...
       </button>
